410357668f7d9816797587d734cbadf875c1ea3a
Mihail Mihov <mmihov.personal@gmail.com>
2018-03-01 15:24:29 +0200

    I will find you, apk

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+        find ~ -type f -name "*.apk"
+
 cp app/*.apk SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001295252259" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001295252259" -F document=@"SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
