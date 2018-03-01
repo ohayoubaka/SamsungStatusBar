ad86a928787a4f7339cbb9b27a85dfa2aaaf08a2
Mihail Mihov <mmihov.personal@gmail.com>
2018-03-01 09:01:54 +0200

    Kanging isn't ez boi

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cp app/build/outputs/apk/debug/app-debug.apk TelegramXMods-$TRAVIS_BUILD_NUMBER.apk
+cp app/build/outputs/apk/debug/app-debug.apk SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001295252259" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001295252259" -F document=@"SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
