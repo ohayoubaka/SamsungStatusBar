5259673fe2b9f0e655047e97a1d0e50b0c4dfbfa
Mihail Mihov <mmihov.personal@gmail.com>
2018-04-14 19:40:56 +0300

    Did I get it right?

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -2,7 +2,7 @@
 
         find ~ -type f -name "*.apk"
 
-cp app/build/outputs/apk/release/app-release-unsigned.apk SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk
+cp app/build/outputs/apk/debug/app-debug.apk SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001295252259" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001295252259" -F document=@"SamsungStatusBar-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
